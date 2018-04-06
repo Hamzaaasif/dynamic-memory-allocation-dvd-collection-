7e31d495d791ba918ea9d702154c29d9f6370bd9
Hamza Asif <hamzaasif0987@gmail.com>
2018-04-06 15:59:21 +0500

    added git ignore

--- main.cpp
+++ main.cpp
@@ -12,9 +12,21 @@ int main()
   d1.add(*(new dvd("CALL OF DUTY",170)));
   d1.add(*(new dvd("CRICKET 2011",100)));
   d1.display();
+  dvdcollection d2;
+  d2.add(*(new dvd("dasc ",150)));
+  d2.add(*(new dvd("Wacdadc",170)));
+  d2.add(*(new dvd("GTA VC",100)));
+  d2.add(*(new dvd("IGI",120)));
+  d2.add(*(new dvd("NEED FOR SPEED",150)));
+  d2.add(*(new dvd("CALL OF DUTY",170)));
+  d2.add(*(new dvd("CRIadcdacdcdc",100)));
+  d2.display();
   cout << endl <<"Index :"<<d1.search(100)<<endl;
   d1.Delete("CRICKET 2011");
   d1.display();
   system("pause");
 
 };
+
+
+//https://github.com/Hamzaaasif/dynamic-memory-allocation-dvd-collection-.git
